a7a55ce15e1781f83d2f4e4e1a109af44348c010
Sergey Chernyakov <chernyakov.sergey@gmail.com>
2026-09-16 11:47:21 +0200

    На десктопе не было входа и ссылки на админку
    «Войти» и «Админка» жили только в бургер-меню, а оно скрыто шире 860 px:
    попасть в админку можно было лишь по прямому адресу. Теперь кнопка питомца
    в шапке ведёт гостя на вход, а администратор видит «Админку» в основном меню.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -9,6 +9,31 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     assert_select ".promo", 3
   end
 
+  test "гостю кнопка в шапке ведёт на вход" do
+    get root_path
+
+    assert_select ".icon-btn[href=?]", new_user_session_path
+    assert_select ".nav__link", { text: "Админка", count: 0 }, "гость не должен видеть админку"
+  end
+
+  test "администратор видит ссылку на админку в шапке" do
+    sign_in_admin
+
+    get root_path
+
+    assert_select ".nav__link[href=?]", admin_root_path, text: "Админка"
+    assert_select ".icon-btn[href=?]", profile_path
+  end
+
+  test "обычный пользователь ссылки на админку не видит" do
+    sign_in_member
+
+    get root_path
+
+    assert_select ".nav__link", { text: "Админка", count: 0 }
+    assert_select ".menu__link", { text: "Админка", count: 0 }
+  end
+
   test "страница о сервисе открывается" do
     get about_path
 
